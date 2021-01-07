372c5c3bd7a6bf17f0b96aeb4675c89ac133c440
ZoltePudeleczko <szzborowski@gmail.com>
2021-01-07 01:35:10 +0100

    Add constraints and padding to desktop navbar

--- lib/Navbar/navbar.dart
+++ lib/Navbar/navbar.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Navbar extends StatelessWidget {
@@ -21,38 +20,56 @@ class Navbar extends StatelessWidget {
 class DesktopNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Text(
-            "Szymon Samuel Zborowski",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 30
-            )
-          ),
-          Row(
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 40
+      ),
+      child: Container(
+        constraints: BoxConstraints(),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text(
-                "Home",
-                style: TextStyle(
-                  color: Colors.white),
+              Row(
+                children: <Widget>[
+                  Text(
+                      "Szymon Samuel ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          fontSize: 30
+                      )
+                  ),
+                  Text(
+                      "Zborowski",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 30
+                      )
+                  ),
+                ]
               ),
-              Text(
-                "About us",
-                style: TextStyle(
-                    color: Colors.white),
-              ),
-              Text(
-                "Portfolio",
-                style: TextStyle(
-                    color: Colors.white),
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Home",
+                    style: TextStyle(
+                      color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Text(
+                    "Portfolio",
+                    style: TextStyle(
+                        color: Colors.white),
+                  )
+                ]
               )
-            ]
-          )
-        ]
-      )
+          ]
+        )
+      ),
     );
   }
 }
