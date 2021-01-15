0a26cb33a8a65284742376e6e536483752dcecb8
ZoltePudeleczko <szzborowski@gmail.com>
2021-01-15 01:02:01 +0100

    Add wave_clipper and create wave with portfolio.dart

--- lib/Portfolio/portfolio.dart
+++ lib/Portfolio/portfolio.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:zborowskihomepage/Funstaf/wave_clipper.dart';
 
 class Portfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (context, constraints) {
-          if (constraints.maxWidth > 1200) { // Big Desktop
-            return DesktopPortfolio();
+          if (constraints.maxWidth > 1400) { // Big Desktop
+            return Padding(
+              padding: const EdgeInsets.only(
+                  top: 200,
+              ),
+              child: DesktopPortfolio(),
+            );
           } else { // Mobile
-            return MobilePortfolio();
+            return Padding(
+              padding: const EdgeInsets.only(
+                  top: 50.0,
+              ),
+              child: MobilePortfolio(),
+            );
           }
         }
     );
@@ -18,13 +29,27 @@ class Portfolio extends StatelessWidget {
 class DesktopPortfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ClipPath(  //upper clippath with less height
+          clipper:WaveClipper(), //set our custom wave clipper.
+          child:Container(
+              color: Colors.white,
+              height: 800,
+              alignment: Alignment.center,
+          ),
+        );
   }
 }
 
 class MobilePortfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ClipPath(  //upper clippath with less height
+      clipper:WaveClipper(), //set our custom wave clipper.
+      child:Container(
+          color: Colors.white,
+          height:800,
+          alignment: Alignment.center,
+      ),
+    );
   }
 }
